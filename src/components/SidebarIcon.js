c955dc16e96814e7240caec4fc37fdaad0d5a249
mitrotasios <a.mitrotasios13@gmail.com>
2021-02-15 19:35:08 +0200

    delete final

--- src/components/SidebarIcon.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import * as FaIcons from 'react-icons/fa';
-
-const SidebarIcon = ({handleClick, isOpen}) => {
-  return <span onClick={handleClick}>
-    {isOpen ? <FaIcons.FaWindowClose /> : <FaIcons.FaBars/>}
-  </span>
-}
-export default SidebarIcon
