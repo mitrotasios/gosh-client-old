1ef648d529846094a60cc85dbb8191f275b389fc
mitrotasios <a.mitrotasios13@gmail.com>
2021-03-16 14:05:28 +0000

    QR Code generator/downloader

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -9,6 +9,8 @@ import { Button} from 'reactstrap';
 import { Checkbox } from './CheckBox';
 import {AiFillCaretDown, AiFillCaretUp, AiOutlineGroup, AiOutlineUngroup, AiOutlineRight, AiOutlineDown} from 'react-icons/ai';
 import QRCode  from 'qrcode.react';
+import { Portal } from 'react-portal';
+import html2canvas from 'html2canvas';
 
 export const Inventory = (props) => {
     
@@ -94,7 +96,7 @@ export const Inventory = (props) => {
         });
     }
 
-    //const downloadQR = () => {
+    const downloadQR = () => {
         // const renderQR = () => {
         //     return(
         //         <div style={{display: "none"}}>
@@ -111,25 +113,33 @@ export const Inventory = (props) => {
         // }
 
         // const startDownload = () => {
-        //     const canvas = document.getElementById("123456");
-        //     const pngUrl = canvas
-        //         .toDataURL("image/png")
-        //         .replace("image/png", "image/octet-stream");
-        //     let downloadLink = document.createElement("a");
-        //     downloadLink.href = pngUrl;
-        //     downloadLink.download = "123456.png";
-        //     document.body.appendChild(downloadLink);
-        //     downloadLink.click();
-        //     document.body.removeChild(downloadLink);
-        //     document.body.removeChild(canvas);
-        // }
+        
+        document.getElementById("hidden-qr").style.display = "block";
+        document.getElementById("hidden-qr").style.marginTop = "1500px";
 
-        // selectedFlatRows.forEach(row => {
-        //     renderQR()
-        //     startDownload()
-        // })
+        selectedFlatRows.forEach(row => {
+            if (row.original != null) {
+                var elemId = String(row.original.id)+"-ext" 
+                var elem = document.getElementById(`${elemId}`)
+                elem.style.display = "block";           
+                html2canvas(elem).then(function(canvas) {
+                    elem.style.display = "none";  
+                    const pngUrl = canvas
+                        .toDataURL("image/png")
+                        .replace("image/png", "image/octet-stream");  
+                    let downloadLink = document.createElement("a");
+                    downloadLink.href = pngUrl;
+                    downloadLink.download = elemId+".png";
+                    document.body.appendChild(downloadLink);
+                    downloadLink.click();
+                    document.body.removeChild(downloadLink);                                   
+                })    
+            }            
+        });
 
-    //}
+        document.getElementById("hidden-qr").style.display = "none";
+        document.getElementById("hidden-qr").style.marginTop = "0px";
+    }
     
     const renderRowSubComponent = React.useCallback(
         ({ row }) => (
@@ -148,22 +158,13 @@ export const Inventory = (props) => {
         <>                
         <div id="page-wrap" className="container-fluid">
             <div className="container-fluid">
-                <div className="row text-center"> 
-                <div style={{display: "none"}}>
-                     <QRCode
-                        id="123456"
-                        value="123456"
-                        size={290}
-                        level={"H"}
-                        includeMargin={true}
-                    />
-                </div>                
+                <div className="row text-center">                             
                     <img id="logo" src="/assets/images/GOSH.png" height="40px" width="200px"/>                    
                     <div className="col-sm-4 ml-auto">
                         <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter}/>
                     </div>                 
                     <div className="ml-auto">
-                        {selectedFlatRows[0] ? (<span><Button className="btn-styled">Print QR</Button> </span>) : <span><Button className="btn btn-white" disabled>Print QR</Button> </span>}
+                        {selectedFlatRows[0] ? (<span><Button onClick={downloadQR} className="btn-styled">Print QR</Button> </span>) : <span><Button className="btn btn-white" disabled>Print QR</Button> </span>}
                         {selectedFlatRows[0] ? (<span><Button className="btn" color="danger"
                             onClick={deleteRows}>Delete</Button> </span>) : <span><Button className="btn btn-white" disabled>Delete</Button> </span>}
                         <span><Button className="btn-styled" 
@@ -249,6 +250,49 @@ export const Inventory = (props) => {
                     })}
                     </tbody>
                 </table>
+                {/* <pre>
+                    <code>
+                        {JSON.stringify({
+                            selectedFlatRows: selectedFlatRows.map((row) => row.original),
+                        },
+                        null,
+                        2
+                        )}
+                    </code>
+                </pre> */}
+                <div id="hidden-qr">                    
+                    {
+                        selectedFlatRows.map(row => {
+                            if (row.original == null) {
+                                return <div></div>
+                            } 
+                            else {
+                                return( 
+                                    <div style={{display: "none"}} key={row.original.id} id={String(row.original.id)+"-ext"} className="container">
+                                        <div className="row">                                            
+                                            <h5><b>LOT Number</b>: {row.original.lot_number} {"\n"}</h5>                                                                                                                                                                                            
+                                        </div>               
+                                        <div className="row">                                    
+                                            <QRCode
+                                                id={String(row.original.id)}
+                                                value={String(row.original.id)}
+                                                size={290}
+                                                level={"H"}
+                                                includeMargin={false}
+                                            />
+                                        </div>              
+                                        <div className="row">
+                                            <p><b>Unique ID</b>: {row.original.id}</p>{' '}    
+                                        </div>                        
+                                        <div className="row">
+                                            <p><b>Pack No</b>: 1/5</p>                                                                                                    
+                                        </div>                                                                                           
+                                    </div>
+                                );
+                            }                                        
+                        })
+                    }
+                </div>
                 <AddReagent isSidebarOpen={isSidebarOpen} onSidebarToggle={toggleSidebar} 
                     selectedRow={{selectedFlatRows: selectedFlatRows.map((row) => row.original)[0]}}
                     resetAddReagentForm={props.resetAddReagentForm}
