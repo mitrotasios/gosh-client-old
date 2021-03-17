0f844fa82ec8b8c81254c34c1a82ed652adc2aaf
mitrotasios <a.mitrotasios13@gmail.com>
2021-03-17 18:51:02 +0000

    minor fixes

--- src/components/AddReagent.js
+++ src/components/AddReagent.js
@@ -38,8 +38,6 @@ class AddReagent extends Component {
     }
 
     handleSubmit(values) {
-        console.log("Current State is: " + JSON.stringify(values));
-        alert("Current State is: " + JSON.stringify(values));
         var expDate = "2010-01-01T23:56:02Z";
         var receivedDate = "2010-01-01T23:56:02Z";
 
